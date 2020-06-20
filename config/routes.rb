fb1b5483d0dce5d7a9397f051b5dc03b4e1d5ba8
Mirko <19408385+moekify@users.noreply.github.com>
2020-06-20 16:17:02 +0200

    Adding devise authentication and user model to the application

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    get 'sign_in', to: 'users/sessions#new'
+    get 'sign_up', to: 'users/registrations#new'
+    get 'forgot_password', to: 'users/passwords#new'
+    get 'reset_password', to: 'users/passwords#edit'
+  end
+
+  root to: 'home#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
---
+++ spec/integration/user_sign_in_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'users', type: :feature do
+
+  describe 'user signs up' do
+    it 'allows users to sign up' do
+      visit '/users/sign_up'
+
+      fill_in 'Email', with: 'test@test.com'
+      fill_in 'Password', with: 'all1needislove'
+      fill_in 'Password confirmation', with: 'all1needislove'
+
+      click_button 'Sign up'
+
+      expect(page).to have_content('Welcome! You have signed up successfully.')
+    end
+
+  end
+
+  describe 'user logs in' do
+    it 'allows an existing user to sign in' do
+      User.create!(email: 'me@home.com', password: 'watching the telly')
+
+      visit '/users/sign_in'
+
+      fill_in 'Email', with: 'me@home.com'
+      fill_in 'Password', with: 'watching the telly'
+
+      click_button 'Log in'
+
+      expect(page).to have_content('Signed in successfully.')
+    end
+  end
+
+end
