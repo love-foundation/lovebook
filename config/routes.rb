2ada9a758eba7d112e7a510eae35054f94700d8e
Mirko <19408385+moekify@users.noreply.github.com>
2021-03-15 14:28:15 +0100

    Handle show route visits for expenses

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
-Rails.application.routes.draw do
-  resources :events
-  resources :expenses
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations'
-  }
-  devise_scope :user do
-    get 'sign_in', to: 'users/sessions#new'
-    get 'sign_up', to: 'users/registrations#new'
-    get 'forgot_password', to: 'users/passwords#new'
-    get 'reset_password', to: 'users/passwords#edit'
+Rails
+  .application
+  .routes
+  .draw do
+    resources :events
+    resources :expenses, except: :show
+    devise_for :users,
+               controllers: {
+                 sessions: 'users/sessions',
+                 registrations: 'users/registrations',
+               }
+    devise_scope :user do
+      get 'sign_in', to: 'users/sessions#new'
+      get 'sign_up', to: 'users/registrations#new'
+      get 'forgot_password', to: 'users/passwords#new'
+      get 'reset_password', to: 'users/passwords#edit'
+    end
+
+    root to: 'home#index'
+
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   end
-
-  root to: 'home#index'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
