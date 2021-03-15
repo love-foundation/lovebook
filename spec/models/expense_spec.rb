38664da77cff2d0bdf6de7e2b3b9a7350b42a3be
Mirko <19408385+moekify@users.noreply.github.com>
2021-03-15 11:15:20 +0100

    Add the basic expense model

---
+++ spec/models/expense_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Expense, type: :model do
+  it { is_expected.to validate_presence_of(:name) }
+
+  it { is_expected.to validate_presence_of(:amount) }
+
+  it { is_expected.to validate_numericality_of(:amount) }
+end
