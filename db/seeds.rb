f1eb11f7542e06bb8465a4723d62b79378f8f0ce
Mirko <19408385+moekify@users.noreply.github.com>
2021-03-15 13:22:48 +0100

    Add an expense index view
    This commits includes some seed expenses as well as the index page for seeing expenses

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+users =
+  User.create!(
+    [
+      email: 'admin@love-foundation.org',
+      password: 'all1needislove',
+      hub: Hub.first,
+      firstname: 'Admin',
+      lastname: 'Gudelaunus',
+    ],
+  )
+
 hubs =
-  Hub.create(
+  Hub.create!(
     [
       { city: 'Berlin', country: 'Germany' },
       { city: 'Dresden', country: 'Germany' },
@@ -20,10 +31,66 @@ hubs =
   )
 
 events =
-    Event.create([
-      {name: 'Awesome Party at KitKat', location: 'Kitkat', date: Time.now, hub: Hub.first},
-      {name: 'Awesome Party at Berghain', location: 'Berghain', date: Time.now, hub: Hub.second},
-      {name: 'Awesome Party at Mensch Meier', location: 'Mensch Meier', date: Time.now, hub: Hub.last}
-    ])
+  Event.create!(
+    [
+      {
+        name: 'Awesome Party at KitKat',
+        location: 'Kitkat',
+        date: Time.now,
+        hub: Hub.first,
+      },
+      {
+        name: 'Awesome Party at Berghain',
+        location: 'Berghain',
+        date: Time.now,
+        hub: Hub.second,
+      },
+      {
+        name: 'Awesome Party at Mensch Meier',
+        location: 'Mensch Meier',
+        date: Time.now,
+        hub: Hub.last,
+      },
+    ],
+  )
 
-users = User.create([email: "admin@love-foundation.org", password: "all1needislove", hub: Hub.first])
+expenses =
+  Expense.create!(
+    [
+      {
+        name: 'DJ Travel Cost',
+        description: 'Train Tickets',
+        amount: 221.20,
+        event: Event.first,
+        user: User.first,
+      },
+      {
+        name: 'Fruit o Clock',
+        description: 'Fruits',
+        amount: 21.87,
+        event: Event.first,
+        user: User.first,
+      },
+      {
+        name: 'Big costs',
+        description: 'Great great costs',
+        amount: 123.45,
+        event: Event.first,
+        user: User.first,
+      },
+      {
+        name: 'All dem moneys',
+        description: 'More moneys',
+        amount: 54.321,
+        event: Event.first,
+        user: User.first,
+      },
+      {
+        name: 'Other Expense',
+        description: 'Dis expense',
+        amount: 213.87,
+        event: Event.last,
+        user: User.first,
+      },
+    ],
+  )
